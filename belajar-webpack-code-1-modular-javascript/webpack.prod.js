a5f60a267c4a3e4a9e8d80ea05100967c60f2d3e
Randi Febriadi <randifebriadi@gmail.com>
2025-01-22 18:37:14 +0700

    Minimizer

--- belajar-webpack-code-1-modular-javascript/webpack.prod.js
+++ belajar-webpack-code-1-modular-javascript/webpack.prod.js
@@ -1,7 +1,15 @@
 const path = require("path");
+const glob = require("glob");
 const config = require("./webpack.config");
 const { merge } = require("webpack-merge");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+const PATHS = {
+  src: path.join(__dirname, "src"),
+};
 
 module.exports = merge(config, {
   mode: "production",
@@ -11,9 +19,29 @@ module.exports = merge(config, {
     assetModuleFilename: "img/[hash][ext]",
     clean: true,
   },
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.sharpMinify,
+          options: {
+            encodeOptions: {
+              // Your options for `sharp`
+              // https://sharp.pixelplumbing.com/api-output
+            },
+          },
+        },
+      }),
+    ],
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: "main.[contenthash].css",
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
     }),
   ],
 });
