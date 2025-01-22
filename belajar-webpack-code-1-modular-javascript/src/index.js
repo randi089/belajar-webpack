f9c80e6002b4f2d9e6b11a0df9b289b200c24716
Randi Febriadi <randifebriadi@gmail.com>
2025-01-22 14:54:33 +0700

    loader

--- belajar-webpack-code-1-modular-javascript/src/index.js
+++ belajar-webpack-code-1-modular-javascript/src/index.js
@@ -1,3 +1,6 @@
+import * as bootstrap from "bootstrap";
+// require("./style.css"); or
+import css from "./style.scss";
 import { run } from "./app/app.js";
 import { AlertService } from "./app/alert.service.js";
 import { CalculatorService } from "./app/calculator.service.js";
--- belajar-webpack-code-1-modular-javascript/webpack.config.js
+++ belajar-webpack-code-1-modular-javascript/webpack.config.js
@@ -9,4 +9,29 @@ module.exports = {
   },
   watch: true,
   devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env"]],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 };
